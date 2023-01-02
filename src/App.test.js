ca2b259d53f90ca5443af7b0c7ade2e6377d6afa
Bueno <luizhenriquebueno@me.com>
2023-01-02 11:48:24 -0500

    Adding another test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App", () => {
+  it("should render a learn react link", () => {
+    render(<App />);
+
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("should render a github actions link", () => {
+    render(<App />);
+
+    const linkElement = screen.getByText(/learn gitHub actions/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
